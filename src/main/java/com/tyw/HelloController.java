a17606ba6edbf5c64112c485820794b5a3193bee
tboss <>
2017-11-10 21:35:18 +0800

    配置jpa连接数据库

--- src/main/java/com/tyw/HelloController.java
+++ src/main/java/com/tyw/HelloController.java
@@ -3,6 +3,8 @@ package com.tyw;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,8 +17,12 @@ public class HelloController {
     @Autowired
     private Boyproperties boyproperties;
 
-    @GetMapping("/hello")
-    public String say(){
-        return say + "," + boyproperties.getName();
+    @GetMapping({"/hello","/hi"})
+    public String hello(){
+        return say + boyproperties.getName();
+    }
+    @PostMapping({"/say/{word}"})
+    public String say(@PathVariable(value = "word",required = false) String word){
+        return "Hello " + word ;
     }
 }
---
+++ src/main/java/com/tyw/User.java
@@ -0,0 +1,72 @@
+package com.tyw;
+import javax.persistence.Entity;
+
+import javax.persistence.GeneratedValue;
+
+import javax.persistence.Id;
+import java.util.Date;
+
+/**
+ * Created by tboss on 2017/11/10.
+ */
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private Long Id;
+    private String userName;
+    private String password;
+    private Date createTime;
+    private Date updateTime;
+    private Integer state;
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+
+    public User() {
+    }
+}
